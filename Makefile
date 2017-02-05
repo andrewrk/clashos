140edbe624520efe422cac33930086229cf9a5e5
Andrew Kelley <superjoe30@gmail.com>
2017-02-05 02:15:19 -0500

    now it's a raspberry pi hello world kernel

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 ZIG=/home/andy/dev/zig/build/zig
-LD=/home/andy/local/i386/bin/i686-elf-ld
-AR=/home/andy/local/i386/bin/i686-elf-ar
-
+LD=/home/andy/local/arm-gnueabihf/bin/arm-linux-gnueabihf-ld
+AR=/home/andy/local/arm-gnueabihf/bin/arm-linux-gnueabihf-ar
 
 all:
-	$(ZIG) build kernel.zig --export exe --name clashos --target-os freestanding --target-arch i386 --linker-script linker.ld --ld-path $(LD) --ar-path $(AR) --static --release
-	grub-file --is-x86-multiboot clashos
-	mkdir -p out/iso/boot/grub
-	cp grub.cfg out/iso/boot/grub/grub.cfg
-	cp clashos out/iso/boot/clashos.bin
-	grub-mkrescue -o clashos.iso out/iso/
+	$(ZIG) build kernel.zig --export exe --name clashos --target-os freestanding --target-arch armv7 --target-environ gnueabihf --linker-script linker.ld --ld-path $(LD) --ar-path $(AR) --release
+
+test: all
+	qemu-system-arm -kernel clashos -m 256 -M raspi2 -serial stdio
+
+clean:
+	rm -f *.o clashos
--- kernel.zig
+++ kernel.zig
@@ -1,119 +1,143 @@
-const MultibootHeader = packed struct {
-    magic: usize,
-    flags: usize,
-    checksum: usize,
-};
+// The linker will make the address of these global variables equal
+// to the value we are interested in. The memory at the address
+// could alias any uninitialized global variable in the kernel.
+extern var __bss_start: u8;
+extern var __bss_end: u8;
 
-const MAGIC   = usize(0x1badb002); // so bootloader can find header
-const ALIGN   = usize(0b0);     // align loaded modules on page boundaries
-const MEMINFO = usize(0b1);     // provide memory map
-const FLAGS   = ALIGN | MEMINFO;
+// r0 -> 0x00000000
+// r1 -> 0x00000C42
+// r2 -> 0x00000100 - start of ATAGS
+// r15 -> should begin execution at 0x8000.
+export nakedcc fn _start(r0: usize, r1: usize, atags: usize) -> unreachable {
+    // to keep this in the first portion of the binary
+    // TODO
+    //@setGlobalSection(_start, ".text.boot");
 
-export const multiboot_header = {
-    @setGlobalSection(multiboot_header, ".multiboot");
-    @setGlobalAlign(multiboot_header, 4);
+    // set up the stack
+    asm volatile ("mov sp, #0x8000");
 
-    MultibootHeader {
-        .magic = MAGIC,
-        .flags = FLAGS,
-        .checksum = ~(MAGIC +% FLAGS) +% 1,
-    }
-};
+    // clear .bss
+    @memset(&__bss_start, 0, usize(&__bss_end) - usize(&__bss_start));
 
-var stack: [16 * 1024]u8 = {
-    @setGlobalAlign(stack, 16);
-    undefined
-};
-var stack_top: usize = undefined;
-
-export nakedcc fn _start() -> unreachable {
-    stack_top = usize(&stack) + stack.len;
-
-    // set up the stack by pointing the esp register at the top
-    // stack grows downwards on x86 systems
-    asm volatile (""
-        :
-        : [stack_top] "{esp}" (stack_top)
-        );
-
-    kernel_main();
-
-    // hang
-    while (true) {}
+    kernel_main(r0, r1, atags);
+    halt();
 }
 
-// Hardware text mode color constants
-const VgaColor = u8; // TODO make a typedef
-const VGA_COLOR_BLACK = 0;
-const VGA_COLOR_BLUE = 1;
-const VGA_COLOR_GREEN = 2;
-const VGA_COLOR_CYAN = 3;
-const VGA_COLOR_RED = 4;
-const VGA_COLOR_MAGENTA = 5;
-const VGA_COLOR_BROWN = 6;
-const VGA_COLOR_LIGHT_GREY = 7;
-const VGA_COLOR_DARK_GREY = 8;
-const VGA_COLOR_LIGHT_BLUE = 9;
-const VGA_COLOR_LIGHT_GREEN = 10;
-const VGA_COLOR_LIGHT_CYAN = 11;
-const VGA_COLOR_LIGHT_RED = 12;
-const VGA_COLOR_LIGHT_MAGENTA = 13;
-const VGA_COLOR_LIGHT_BROWN = 14;
-const VGA_COLOR_WHITE = 15;
- 
-fn vga_entry_color(fg: VgaColor, bg: VgaColor) -> u8 {
-    return fg | (bg << 4);
+fn halt() -> unreachable {
+    while (true) { }
 }
- 
-fn vga_entry(uc: u8, color: u8) -> u16 {
-    return u16(uc) | (u16(color) << 8);
+
+fn mmio_write(reg: usize, data: u32) {
+    *(&volatile u32)(reg) = data;
 }
- 
-const VGA_WIDTH = 80;
-const VGA_HEIGHT = 25;
 
-const terminal = struct {
-    var row = usize(0);
-    var column = usize(0);
-    var color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+fn mmio_read(reg: usize) -> u32 {
+    return *(&volatile usize)(reg);
+}
 
-    const buffer = (&u16)(0xB8000);
+// Loop count times in a way that the compiler won't optimize away.
+fn delay(count: usize) {
+    var i: usize = 0;
+    while (i < count; i += 1) {
+        asm volatile("mov r0, r0");
+    }
+}
 
-    fn initialize() {
-        {var y = usize(0); while (y < VGA_HEIGHT; y += 1) {
-            {var x = usize(0); while (x < VGA_WIDTH; x += 1) {
-                putCharAt(' ', color, x, y);
-            }}
-        }}
+// The GPIO registers base address.
+const GPIO_BASE = 0x3F200000;
+
+// The offsets for reach register.
+
+// Controls actuation of pull up/down to ALL GPIO pins.
+const GPPUD = (GPIO_BASE + 0x94);
+
+// Controls actuation of pull up/down for specific GPIO pin.
+const GPPUDCLK0 = (GPIO_BASE + 0x98);
+
+// The base address for UART.
+const UART0_BASE = 0x3F201000;
+
+// The offsets for reach register for the UART.
+const UART0_DR     = (UART0_BASE + 0x00);
+const UART0_RSRECR = (UART0_BASE + 0x04);
+const UART0_FR     = (UART0_BASE + 0x18);
+const UART0_ILPR   = (UART0_BASE + 0x20);
+const UART0_IBRD   = (UART0_BASE + 0x24);
+const UART0_FBRD   = (UART0_BASE + 0x28);
+const UART0_LCRH   = (UART0_BASE + 0x2C);
+const UART0_CR     = (UART0_BASE + 0x30);
+const UART0_IFLS   = (UART0_BASE + 0x34);
+const UART0_IMSC   = (UART0_BASE + 0x38);
+const UART0_RIS    = (UART0_BASE + 0x3C);
+const UART0_MIS    = (UART0_BASE + 0x40);
+const UART0_ICR    = (UART0_BASE + 0x44);
+const UART0_DMACR  = (UART0_BASE + 0x48);
+const UART0_ITCR   = (UART0_BASE + 0x80);
+const UART0_ITIP   = (UART0_BASE + 0x84);
+const UART0_ITOP   = (UART0_BASE + 0x88);
+const UART0_TDR    = (UART0_BASE + 0x8C);
+
+fn uart_init() {
+    // Disable UART0.
+    mmio_write(UART0_CR, 0x00000000);
+    // Setup the GPIO pin 14 && 15.
+
+    // Disable pull up/down for all GPIO pins & delay for 150 cycles.
+    mmio_write(GPPUD, 0x00000000);
+    delay(150);
+
+    // Disable pull up/down for pin 14,15 & delay for 150 cycles.
+    mmio_write(GPPUDCLK0, (1 << 14) | (1 << 15));
+    delay(150);
+
+    // Write 0 to GPPUDCLK0 to make it take effect.
+    mmio_write(GPPUDCLK0, 0x00000000);
+
+    // Clear pending interrupts.
+    mmio_write(UART0_ICR, 0x7FF);
+
+    // Set integer & fractional part of baud rate.
+    // Divider = UART_CLOCK/(16 * Baud)
+    // Fraction part register = (Fractional part * 64) + 0.5
+    // UART_CLOCK = 3000000; Baud = 115200.
+
+    // Divider = 3000000 / (16 * 115200) = 1.627 = ~1.
+    // Fractional part register = (.627 * 64) + 0.5 = 40.6 = ~40.
+    mmio_write(UART0_IBRD, 1);
+    mmio_write(UART0_FBRD, 40);
+
+    // Enable FIFO & 8 bit data transmissio (1 stop bit, no parity).
+    mmio_write(UART0_LCRH, (1 << 4) | (1 << 5) | (1 << 6));
+
+    // Mask all interrupts.
+    mmio_write(UART0_IMSC, (1 << 1) | (1 << 4) | (1 << 5) | (1 << 6) |
+                           (1 << 7) | (1 << 8) | (1 << 9) | (1 << 10));
+
+    // Enable UART0, receive & transfer part of UART.
+    mmio_write(UART0_CR, (1 << 0) | (1 << 8) | (1 << 9));
+}
+
+fn uart_putc(byte: u8) {
+    // Wait for UART to become ready to transmit.
+    while ( (mmio_read(UART0_FR) & (1 << 5)) != 0 ) { }
+    mmio_write(UART0_DR, byte);
+}
+
+fn uart_getc() -> u8 {
+    // Wait for UART to have recieved something.
+    while ( (mmio_read(UART0_FR) & (1 << 4)) != 0 ) { }
+    return @truncate(u8, mmio_read(UART0_DR));
+}
+
+fn uart_write(buffer: []u8) {
+    for (buffer) |c| uart_putc(c);
+}
+
+fn kernel_main(r0: u32, r1: u32, atags: u32) {
+    uart_init();
+    uart_write("Hello, kernel World!\r\n");
+
+    while (true) {
+        uart_putc(uart_getc());
     }
-    
-    fn setColor(new_color: u8) {
-        color = new_color;
-    }
-    
-    fn putCharAt(c: u8, new_color: u8, x: usize, y: usize) {
-        const index = y * VGA_WIDTH + x;
-        @volatileStore(&buffer[index], vga_entry(c, new_color));
-    }
-    
-    fn putChar(c: u8) {
-        putCharAt(c, color, column, row);
-        column += 1;
-        if (column == VGA_WIDTH) {
-            column = 0;
-            row += 1;
-            if (row == VGA_HEIGHT)
-                row = 0;
-        }
-    }
-    
-    fn write(data: []const u8) {
-        for (data) |c| putChar(c);
-    }
- 
-};
- 
-fn kernel_main() {
-    terminal.initialize();
-    terminal.write("Hello, kernel World!\n");
 }
