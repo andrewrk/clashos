e2d3039f2ff6552382e6aaa0c028ef669a0440d0
Andrew Kelley <superjoe30@gmail.com>
2017-04-18 00:05:38 -0400

    update to latest zig

--- src/int_types.zig
+++ src/int_types.zig
@@ -1,19 +1,19 @@
-pub const u1 = @intType(false, 1);
-pub const u2 = @intType(false, 2);
-pub const u3 = @intType(false, 3);
-pub const u4 = @intType(false, 4);
-pub const u5 = @intType(false, 5);
-pub const u6 = @intType(false, 6);
-pub const u7 = @intType(false, 7);
+pub const u1 = @IntType(false, 1);
+pub const u2 = @IntType(false, 2);
+pub const u3 = @IntType(false, 3);
+pub const u4 = @IntType(false, 4);
+pub const u5 = @IntType(false, 5);
+pub const u6 = @IntType(false, 6);
+pub const u7 = @IntType(false, 7);
 
-pub const u9 = @intType(false, 9);
-pub const u10 = @intType(false, 10);
-pub const u11 = @intType(false, 11);
-pub const u12 = @intType(false, 12);
-pub const u13 = @intType(false, 13);
-pub const u14 = @intType(false, 14);
-pub const u15 = @intType(false, 15);
+pub const u9 = @IntType(false, 9);
+pub const u10 = @IntType(false, 10);
+pub const u11 = @IntType(false, 11);
+pub const u12 = @IntType(false, 12);
+pub const u13 = @IntType(false, 13);
+pub const u14 = @IntType(false, 14);
+pub const u15 = @IntType(false, 15);
 
-pub const u18 = @intType(false, 18);
-pub const u19 = @intType(false, 19);
-pub const u24 = @intType(false, 24);
+pub const u18 = @IntType(false, 18);
+pub const u19 = @IntType(false, 19);
+pub const u24 = @IntType(false, 24);
